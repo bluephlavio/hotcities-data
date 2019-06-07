1f2000c53be421e98f4b0c907aa2b0da6882dd5e
Flavio Grandin <flavio.grandin@gmail.com>
2019-06-07 16:18:00 +0200

    merge basic function implemented and data files registry restructured

--- src/filter.py
+++ src/filter.py
@@ -1,11 +1,20 @@
-from .helpers import read_data, data_dir
+from .helpers import load, default_data_files
 
-def filter_cities_by_population(min_population, cities_file, header_file):
-	cities = read_data(cities_file, header_file, filter=lambda city: city['population'] >= min_population)
+def filter_cities_by_population(min_population, data_files=default_data_files()):
+	cities = load('cities', data_files=data_files, where=lambda city: city['population'] >= min_population)
 	return cities
 
-def filter_alternate_names_for_cities(cities, alternate_names_file, header_file):
+def filter_alternate_names_for_cities(cities, data_files=default_data_files()):
 	geonameids = list(map(lambda city: city['geonameid'], cities))
-	filter = lambda alternate_name: alternate_name['geonameid'] in geonameids
-	alternate_names = read_data(alternate_names_file, header_file, filter=filter)
+	def is_ok(name):
+		return (
+			name['isShortName'] != 1 and 
+			name['isHistoric'] != 1 and
+			name['isColloquial'] != 1 and
+			name['isolanguage'] != '' and
+			name['isolanguage'] != 'link' and
+			name['geonameid'] in geonameids
+		)
+	alternate_names = load('alternate_names', data_files=data_files, where=is_ok)
 	return alternate_names
+
