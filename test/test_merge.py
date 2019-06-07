384978ee19175a357bc14f1627fe634091798a69
Flavio Grandin <flavio.grandin@gmail.com>
2019-06-07 16:58:02 +0200

    bug fix in merge_city_data function naming

--- test/test_merge.py
+++ test/test_merge.py
@@ -1,5 +1,5 @@
 from manager.helpers import load
-from manager.merge import merge_city_info
+from manager.merge import merge_city_data
 
 def test_merge_city_data(test_data_files):
 	cities = load('cities', data_files=test_data_files)
