efe3cf555b12801bb4a368e92f3e8bd756e20252
Flavio Grandin <flavio.grandin@gmail.com>
2019-06-09 19:40:54 +0200

    cli extract function fixed and config-file options added

--- manager/cli.py
+++ manager/cli.py
@@ -2,23 +2,27 @@ import argparse
 import sys
 import csv
 
-from .merge import merge_city_data, merged_data_fields
-from .filter import filter_cities_by_population
+from .config import read_config, default_config
+from .mergers import merge_cities_data
+from .filters import is_bigger_than, is_relevant_alternatename_for_any_of
+from .readers import load
 
 def extract(args):
-	cities_table = filter_cities_by_population(args.min_population)
-	cities = list(map(merge_city_data, cities_table))
-	fieldnames = merged_data_fields()
+	config = read_config(args.config_file) if args.config_file else default_config
+	cities = load('cities', where=is_bigger_than(args.min_population), config=config)
+	geonameids = list(map(lambda city: city['geonameid'], cities))
+	countries = load('countries', config=config)
+	alternatenames = load('alternatenames', where=is_relevant_alternatename_for_any_of(geonameids), config=config)
+	data, fields = merge_cities_data(cities, countries, alternatenames)
 	if args.out_file:
 		with open(args.out_file, 'w', newline='', encoding='utf-8') as f:
-			writer = csv.DictWriter(f, fieldnames=fieldnames)
+			writer = csv.DictWriter(f, fieldnames=fields)
 			writer.writeheader()
-			for city in cities:
+			for city in data:
 				writer.writerow(city)
 	else:
-		print(','.join(fieldnames))
-		for city in cities:
-			values_list = list(map(lambda field: str(city[field]), fieldnames))
+		for city in data:
+			values_list = list(map(lambda field: str(city[field]), fields))
 			print(','.join(values_list))
 
 def main():
@@ -28,6 +32,7 @@ def main():
 	extract_subparser = subparsers.add_parser('extract')
 	extract_subparser.add_argument('--min-population', type=int, default=10000000)
 	extract_subparser.add_argument('--out-file', default=None)
+	extract_subparser.add_argument('--config-file', default=None)
 	extract_subparser.set_defaults(func=extract)
 
 	args = parser.parse_args()
