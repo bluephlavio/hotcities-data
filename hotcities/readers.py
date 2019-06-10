dbe861f3890bd0ccb45339af503db661ac466b88
Flavio Grandin <flavio.grandin@gmail.com>
2019-06-10 08:18:49 +0200

    hook added to read_data and load functions

--- hotcities/readers.py
+++ hotcities/readers.py
@@ -7,20 +7,24 @@ def read_fields(filename):
   with open(filename, encoding='utf-8') as f:
     return [line.strip() for line in f.readlines()]
 
-def read_data(data_file, fields_file, dialect='excel-tab', where=lambda x: True):
+def read_data(data_file, fields_file, dialect='excel-tab', where=lambda x: True, hook=None):
   fields = read_fields(fields_file)
   with open(data_file, encoding='utf-8') as f:
     reader = csv.DictReader(f, fieldnames=fields, dialect=dialect, quoting=csv.QUOTE_NONE)
     rows = []
+    i = 0
     for line in reader:
       row = {}
       for field in fields:
         row[field] = parse(line[field])
       if where and where(row):
         rows.append(row)
+        if hook:
+          hook(row, i)
+        i += 1
     return rows
 
-def load(table, where=lambda x: True, config=default_config):
+def load(table, where=lambda x: True, hook=None, config=default_config):
   data_file = config[f'{table}data']
   fields_file = config[f'{table}fields']
-  return read_data(data_file, fields_file, where=where)
+  return read_data(data_file, fields_file, where=where, hook=hook)
