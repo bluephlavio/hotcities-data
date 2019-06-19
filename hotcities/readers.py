70105b992228d4a44fa555a40cb14680e1f400b4
Flavio Grandin <flavio.grandin@gmail.com>
2019-06-19 11:05:37 +0200

    readers module reimplemented and tested with pandas

--- hotcities/readers.py
+++ hotcities/readers.py
@@ -1,30 +1,19 @@
-import csv
+import pandas as pd
 
-from .parsers import parse
 from .config import default_config
 
-def read_fields(filename):
-  with open(filename, encoding='utf-8') as f:
-    return [line.strip() for line in f.readlines()]
 
-def read_data(data_file, fields_file=None, dialect='excel-tab', where=lambda x: True, hook=None):
-  fields = read_fields(fields_file) if fields_file else None
-  with open(data_file, encoding='utf-8') as f:
-    reader = csv.DictReader(f, fieldnames=fields, dialect=dialect, quoting=csv.QUOTE_NONE)
-    rows = []
-    i = 0
-    for line in reader:
-      row = {}
-      for field in reader.fieldnames:
-        row[field] = parse(line[field])
-      if where and where(row):
-        rows.append(row)
-        if hook:
-          hook(row, i)
-        i += 1
-    return rows
+def read_fields(fields_file):
+    with open(fields_file, encoding='utf-8') as f:
+        return [line.strip() for line in f.readlines()]
 
-def load(table, where=lambda x: True, hook=None, config=default_config):
-  data_file = config[f'{table}data']
-  fields_file = config[f'{table}fields']
-  return read_data(data_file, fields_file, where=where, hook=hook)
+
+def read_data(data_file, fields_file=None, delimiter='\t'):
+    fields = read_fields(fields_file) if fields_file else None
+    return pd.read_csv(data_file, names=fields, header=None if fields else 0, delimiter=delimiter)
+
+
+def load(table, config=default_config):
+    data_file = config[f'{table}data']
+    fields_file = config[f'{table}fields']
+    return read_data(data_file, fields_file)
--- test/conftest.py
+++ test/conftest.py
@@ -1,25 +1,28 @@
+from hotcities.config import read_config
 import pytest
-import os.path
+import os
 import sys
 
 here = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, here)
 
-from hotcities.config import read_config
 
 @pytest.fixture(scope='session')
 def res_dir():
-  here = os.path.abspath(os.path.dirname(__file__))
-  return os.path.join(here, 'res' + os.path.sep)
+    here = os.path.abspath(os.path.dirname(__file__))
+    return os.path.join(here, 'res' + os.path.sep)
+
 
 @pytest.fixture(scope='session')
 def fields_file(res_dir):
-  return os.path.join(res_dir, 'fields.txt')
+    return os.path.join(res_dir, 'fields.txt')
+
 
 @pytest.fixture(scope='session')
 def data_file(res_dir):
-  return os.path.join(res_dir, 'data.txt')
+    return os.path.join(res_dir, 'data.txt')
+
 
 @pytest.fixture
 def config(res_dir):
-  return read_config(os.path.join(res_dir, 'config.test.ini'))
+    return read_config(os.path.join(res_dir, 'config.test.ini'))
