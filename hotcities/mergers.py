6b11d966e1d93be74a4de231427e77f1b0ef672e
Flavio Grandin <flavio.grandin@gmail.com>
2019-06-28 07:17:54 +0200

    mergers module cleaned

--- hotcities/mergers.py
+++ hotcities/mergers.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 
 # pd.options.mode.chained_assignment = None
@@ -6,8 +5,8 @@ import pandas as pd
 
 def merge(cities, countries, alternatenames):
     cities_with_lang = pd.merge(cities, countries, on='countrycode')
-    cities_with_localname = pd.merge(
+    df = pd.merge(
         cities_with_lang, alternatenames, how='left', on=['geonameid', 'lang'])
-    cities_with_localname.rename(
+    df.rename(
         columns={'alternatename': 'localname'}, inplace=True)
-    return cities_with_localname
+    return df
