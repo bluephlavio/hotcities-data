e49648d788014dc50149e6a18428ff7c9897aeba
Flavio Grandin <flavio.grandin@gmail.com>
2019-06-28 07:18:11 +0200

    mergers module cleaned

--- hotcities/mergers.py
+++ hotcities/mergers.py
@@ -1,7 +1,5 @@
 import pandas as pd
 
-# pd.options.mode.chained_assignment = None
-
 
 def merge(cities, countries, alternatenames):
     cities_with_lang = pd.merge(cities, countries, on='countrycode')
